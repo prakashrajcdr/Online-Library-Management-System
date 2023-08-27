cf66ff601c565eae05ddf9430386aa24d271fc55
prakashrajcdr <prakashrajcdr@gmail.com>
2023-08-27 14:18:07 +0530

    test commit

--- src/main/java/com/prakash/OlmsApplication.java
+++ src/main/java/com/prakash/OlmsApplication.java
@@ -9,6 +9,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 public class OlmsApplication {
 
 	public static void main(String[] args) {
+		System.out.println("I am from OLMS Application");
 		SpringApplication.run(OlmsApplication.class, args);
 	}
 
