d3b06d4b6f757250d6b79d2676a126f18d9a6899
prakashrajcdr <prakashrajcdr@gmail.com>
2023-08-28 22:54:58 +0530

    to separate service package

--- src/main/java/com/prakash/controller/BooksController.java
+++ src/main/java/com/prakash/controller/BooksController.java
@@ -10,8 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.prakash.BookService;
 import com.prakash.entity.BookDetail;
+import com.prakash.service.BookService;
 
 @RestController
 public class BooksController {
--- src/main/java/com/prakash/service/BookService.java
+++ src/main/java/com/prakash/service/BookService.java
@@ -1,4 +1,4 @@
-package com.prakash;
+package com.prakash.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
