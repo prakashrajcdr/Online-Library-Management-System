1f98a3f43d0ec9d33fba6dec3db8ff4299ecc0e4
prakashrajcdr <prakashrajcdr@gmail.com>
2023-09-12 00:48:34 +0530

    to run it on external tomcat

--- src/main/java/com/prakash/OlmsApplication.java
+++ src/main/java/com/prakash/OlmsApplication.java
@@ -2,15 +2,22 @@ package com.prakash;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
-public class OlmsApplication {
+public class OlmsApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		System.out.println("I am from OLMS Application");
 		SpringApplication.run(OlmsApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return builder.sources(OlmsApplication.class);
+	}
 }
